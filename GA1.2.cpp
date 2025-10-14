f84836c43b8a4d2e6a7379e120d3b2a02bbb08c2
0Waylander0 <levayapochta332@gmail.com>
2025-10-14 17:29:07 +0300

    Add files via upload

---
+++ GA1.2.cpp
@@ -0,0 +1,48 @@
+﻿// GA1.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void selectionSort(int array[], int f) { // Функция сортировки выбором по убыванию
+    for (int i = 0; i < f - 1; ++i) {
+        int maxIndex = i;
+        for (int g = i + 1; g < f; ++g) {
+            if (array[g] > array[maxIndex]) {
+                maxIndex = g;
+            }
+        }
+        if (maxIndex != i) {
+            std::swap(array[i], array[maxIndex]);
+        }
+    }
+}
+
+void printArray(int array[], int f) {
+    for (int i = 0; i < f; ++i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    const int SIZE = 20;
+    int array[SIZE];
+
+    std::srand(static_cast<unsigned int>(std::time(0)));
+
+    for (int i = 0; i < SIZE; ++i) {
+        array[i] = std::rand() % 101;
+    }
+    setlocale(LC_CTYPE, "Russian");
+    std::cout << "Исходный массив:\n";
+    printArray(array, SIZE);
+
+    selectionSort(array, SIZE);
+
+    std::cout << "Массив, отсортированный по убыванию:\n";
+    printArray(array, SIZE);
+
+    return 0;
+}
