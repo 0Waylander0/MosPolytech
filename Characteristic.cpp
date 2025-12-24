606f326257b5b8456878d2fbb96615f74d1212b0
0Waylander0 <levayapochta332@gmail.com>
2025-12-24 21:23:20 +0300

    Delete Characteristic.cpp

--- Characteristic.cpp
+++
@@ -1,8 +0,0 @@
-#include "Characteristic.h"
-
-Characteristic::Characteristic(float strength)
-    : strength(strength) {}
-
-float Characteristic::GetDamage(const Weapon& weapon) {
-    return strength + weapon.damage;
-}
